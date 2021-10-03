86dd31741cd9bf27c1ea58480c9d56d7effdb5bd
AleksandarNe11 <59544247+AleksandarNe11@users.noreply.github.com>
2021-10-03 12:14:01 -0400

    Added testing client method behaviour for TCP headers upon command line input of method types.
    Added testing Server behaviour upon receipt of text based client headers.
    
    Known Bugs:
    The Request class loop awaiting receipt from client seems to continue without any postponement
    
    Need to still:
    Implement and include Board class
    Implement and include Notes class
    Create Server Responses
    Use ObjectInputStream and ObjectOutputStream to transfer the Notes to server and client

--- src/BoardClient.java
+++ src/BoardClient.java
@@ -7,6 +7,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class BoardClient {
+    final static String CRLF = "\r\n";
     public static void main(String[] args) throws IOException {
         Socket socket = null;
         PrintWriter out = null;
@@ -27,7 +28,7 @@ public class BoardClient {
             }
 //        }
 
-        // Reads input from user in command line
+        // Reads input from user in command line for testing setup
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
         String fromServer;
         String fromUser;
@@ -37,15 +38,131 @@ public class BoardClient {
             if (fromServer.equals("Bye."))
                 break;
 
+            // takes command line input, parses input to be one of the method types, sends method evaluation result to server through out.println(request)
             fromUser = stdIn.readLine();
             if (fromUser != null) {
                 System.out.println("Client: " + fromUser);
-                out.println(fromUser);
+                System.out.println("======================================");
+                String request = inputParser(fromUser);
+                System.out.println(request);
+                System.out.println("======================================");
+                out.println(request);
             }
         }
         out.close();
         in.close();
         stdIn.close();
         socket.close();
+    }
+
+    public static String inputParser(String fromUser) {
+        String request;
+        switch(fromUser) {
+            case "POST":
+                request = POST();
+                break;
+            case "GET":
+                request = GET();
+                break;
+            case "PIN":
+                request = PIN();
+                break;
+            case "UNPIN":
+                request = UNPIN();
+                break;
+            case "CLEAR":
+                request = CLEAR();
+                break;
+            case "SHAKE":
+                request = SHAKE();
+                break;
+            case "CONNECT":
+                request = CONNECT();
+                break;
+            default:
+                request = DISCONNECT();
+        }
+        return request;
+    }
+
+    public static String POST() {
+        String method = "POST ";
+        String parameters = "<x coordinate> <y coordinate> <width> <height> <colour> <message> ";
+
+        String header = method.concat(parameters).concat(CRLF);
+//        String body = "<x-coordinate> <y-coordinate> <width> <height> <colour> <message>";
+
+        return header;
+    }
+
+    public static String GET() {
+        String method = "GET ";
+        String parameters = "";
+
+        String header = method.concat(parameters).concat(CRLF);
+
+        return header;
+    }
+
+    public static String GET(String colour, String contains, String refersTo) {
+        String method = "GET ";
+        String parameters = "<colour> <contains> <refersTo>";
+
+        String header = method.concat(parameters).concat(CRLF);
+
+        return header;
+    }
+    public static String PIN() {
+        String method = "PIN ";
+        String parameters = "<x coordinate> <y coordinate>";
+
+        String header = method.concat(parameters).concat(CRLF);
+
+        return header;
+    }
+
+    public static String UNPIN() {
+        String method = "UNPIN ";
+        String parameters = "<x coordinate> <y coordinate>";
+
+        String header = method.concat(parameters).concat(CRLF);
+
+        return header;
+    }
+
+    public static String CLEAR() {
+        String method = "CLEAR ";
+        String parameters = "";
+
+        String header = method.concat(parameters).concat(CRLF);
+
+        return header;
+    }
+
+    public static String SHAKE() {
+        String method = "SHAKE ";
+        String parameters = "";
+
+        String header = method.concat(parameters).concat(CRLF);
+
+        return header;
+    }
+
+    public static String CONNECT() {
+        String method = "CONNECT ";
+        String parameters = "";
+
+        String header = method.concat(parameters).concat(CRLF);
+
+        return header;
+    }
+
+    public static String DISCONNECT() {
+        String method = "DISCONNECT ";
+        String parameters = "";
+
+        String header = method.concat(parameters).concat(CRLF);
+
+        return header;
     }
 }
--- src/BoardRequest.java
+++ src/BoardRequest.java
@@ -22,16 +22,31 @@ public class BoardRequest implements Runnable {
     private void processRequest() throws IOException {
         // Instantiate the input and output streams
         InputStream is = socket.getInputStream();
-        DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+        ObjectOutputStream objectOut = new ObjectOutputStream(socket.getOutputStream());
+        PrintWriter textOut = new PrintWriter(socket.getOutputStream(), true);
+
+        //Send Text to Client
+        textOut.println("Connection Established to server. BoardRequest Running");
 
         // Define a reader to enable request parsing
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        // Parse Header line to get method and call invokeMethod passing Tokenizer for next phase
-        String headerLine = br.readLine();
-        StringTokenizer tokens = new StringTokenizer(headerLine);
-        String method = tokens.nextToken();
-        invokeMethod(method, tokens);
 
+        // Constantly Loop listening for input from the client
+        String headerLine;
+        while ((headerLine = br.readLine()) != null) {
+            // Parse method type and pass string token and method type to invokeMethod for further behaviour
+            StringTokenizer tokens = new StringTokenizer(headerLine);
+            String method = tokens.nextToken();
+            System.out.println(method);
+            invokeMethod(method, tokens);
+            // Send some sort of response to client
+            textOut.println("Please enter next method call");
+        }
+
+        // Close socket connection to client
+        objectOut.close();
+        is.close();
+        socket.close();
     }
 
     private void invokeMethod(String method, StringTokenizer tokens) {
@@ -39,21 +54,28 @@ public class BoardRequest implements Runnable {
             switch(method) {
                 case "POST":
                     POST(tokens);
+                    break;
                 case "GET":
                     GET(tokens);
+                    break;
                 case "PIN":
-                    PIN();
+                    PIN(tokens);
+                    break;
                 case "UNPIN":
-                    UNPIN();
+                    UNPIN(tokens);
+                    break;
                 case "CLEAR":
                     CLEAR();
+                    break;
                 case "SHAKE":
                     SHAKE();
+                    break;
                 case "CONNECT":
                     CONNECT();
+                    break;
                 case "DISCONNECT":
                     DISCONNECT();
-
+                    break;
             }
         } catch (Exception e) {
             System.out.println(e);
@@ -66,6 +88,7 @@ public class BoardRequest implements Runnable {
             while (tokens.hasMoreTokens()) {
                 System.out.println(tokens.nextToken());
             }
+            System.out.println("======================================");
         } else {
             throw new InvalidRequestParametersException();
         }
@@ -74,37 +97,53 @@ public class BoardRequest implements Runnable {
     private void GET(StringTokenizer tokens) throws InvalidRequestParametersException {
         int numTokens = tokens.countTokens();
         if (numTokens == 0) {
-            while (tokens.hasMoreTokens()) {
-                System.out.println("GET request with no request parameters");
-            }
+            System.out.println("GET request with no request parameters");
+            System.out.println("======================================");
         } else if (numTokens == 3) {
             System.out.println("GET request with 3 request parameters");
+            System.out.println("======================================");
         } else {
             throw new InvalidRequestParametersException();
         }
     }
-    private void PIN() {
-
+    private void PIN(StringTokenizer tokens) throws InvalidRequestParametersException {
+        int numTokens = tokens.countTokens();
+        if (numTokens == 2) {
+            System.out.println("PIN request to following coordinates");
+            System.out.printf("x coordinate: %s\n", tokens.nextToken());
+            System.out.printf("y coordinate: %s\n", tokens.nextToken());
+            System.out.println("======================================");
+        } else {
+            throw new InvalidRequestParametersException();
+        }
     }
 
-    private void UNPIN() {
-
+    private void UNPIN(StringTokenizer tokens) throws InvalidRequestParametersException {
+        int numTokens = tokens.countTokens();
+        if (numTokens == 2) {
+            System.out.println("UNPIN request to following coordinates");
+            System.out.printf("x coordinate: %s\n", tokens.nextToken());
+            System.out.printf("y coordinate: %s\n", tokens.nextToken());
+            System.out.println("======================================");
+        } else {
+            throw new InvalidRequestParametersException();
+        }
     }
 
     private void CLEAR() {
-
+        System.out.println("CLEAR request - All notes cleared");
     }
 
     private void SHAKE() {
-
+        System.out.println("SHAKE request - All unpinned notes cleared");
     }
 
     private void CONNECT() {
-
+        System.out.println("I think this method is redundant");
     }
 
     private void DISCONNECT() {
-
+        System.out.println("Disconnecting now");
     }
 }
 
